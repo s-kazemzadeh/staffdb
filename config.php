4c85508e3b9a6deea061d79baadea40dac9c6130
s-kazemzadeh <54800571+s-kazemzadeh@users.noreply.github.com>
2021-05-28 14:37:03 +0200

    Removed config.php | Added .gitignore

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "dc7user";
-	$password = "MdR3xOgB7#dW";
-	$dbname = "Staff";
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
